41d9ff6976e2e50ffb8d336204bca8ebfcaf484a
EstolaKarylle <142649734+EstolaKarylle@users.noreply.github.com>
2023-10-16 21:53:42 +0800

    Add files via upload

---
+++ EstolaProg13.java
@@ -0,0 +1,19 @@
+package IntegerString;
+
+public class EstolaProg13 {
+
+  public static void main(String[] args)
+   {
+   // demonstrating toString() method
+   Integer x = 12;
+   
+   System.out.println(x.toString() );
+   
+   // demonstrating toString(int 1) method
+   System.out.println(Integer. toString(12) );
+   
+    System.out.println(Integer. toBinaryString(152) );
+    System.out.println(Integer. toHexString(152) );
+    System.out.println(Integer. toOctalString(152) );
+   }
+}
