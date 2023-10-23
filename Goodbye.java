b93928e33cf52f42ec1520e92f63954d2c395cb5
EstolaKarylle <142649734+EstolaKarylle@users.noreply.github.com>
2023-10-23 23:27:24 +0800

    Add files via upload

---
+++ Goodbye.java
@@ -0,0 +1,9 @@
+package Unsaid;
+
+public class Goodbye {
+
+  public static void main(String[] args) {
+  System. out. print("You will never have something that was never there");//prints You will never have somthing that was never there
+   
+  }
+}
